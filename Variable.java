947bc005052578a49baf324dc5b74afd06168602
Ashraful Islam <ashrafulcse.ru@gmail.com>
2022-09-09 12:15:38 +0600

    There are 8 primitive Variables and String Variable

---
+++ Variable.java
@@ -0,0 +1,42 @@
+public class Variable {
+    public static void main(String[] args){
+        //8 Primitive Data Types
+
+        boolean flag = true; // true or false
+        System.out.println(flag); // true
+
+        byte b = 124; // -128 to 127 rang
+        System.out.println(b); // 124
+
+        short Short = -1223;  // -32768 to 32767
+        System.out.println(Short);
+
+        int number;
+        number = 938464;
+        System.out.println(number);
+
+        long rang = -42129393939L;
+        System.out.println(rang);
+
+        double Number = -8363.87;
+        System.out.println(Number);
+
+        float Num = -4241f;
+        System.out.println(Num); // -4241
+
+
+        char letter4 = '\u0051';   // rang : \u0000 to \uffff
+        System.out.println(letter4); //  unicode characture  so print Q
+        char letter2 = '9';
+        System.out.println(letter2); // 9
+
+        char letter3 = 65;
+        System.out.println(letter3); // A
+
+        // String Type
+
+        String str = "Ashraful";
+        System.out.println(str);
+
+    }
+}
